c4556907a18a3bf89c7a6b7ce6236e4e29d2600d
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-10 10:55:32 +0000

    building form page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,17 +9,21 @@ function loginCondition(){
       <h1>Hello</h1>
     );
   }
+  else{
+    return(
+      <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+    );
+  }
 }
 
 function App() {
   return (
     <div className="container">
-      
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {loginCondition()}
     </div>
   );
 }
