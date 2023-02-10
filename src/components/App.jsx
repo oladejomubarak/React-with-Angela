3809dd187d473c499216d73089a661195a0c02d1
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-10 10:52:24 +0000

    building form page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+
+var isLoggedIn= true;
+
+function loginCondition(){
+  if (!isLoggedIn){
+    return(
+      <h1>Hello</h1>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
